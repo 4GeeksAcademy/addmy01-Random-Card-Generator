71cb428fa6b21ddab52b0d841f6147723434bf5d
addmy01 <70408689+addmy01@users.noreply.github.com>
2024-02-08 01:13:38 +0000

    first change

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector(".card");
+  let item = document.querySelector("#rankValue");
+
+  let suit = ["hearts", "diamonds", "spades", "clubs"];
+  let rank = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let selectedSuit = suit[getRndInteger(suit.length)];
+
+  card.classList.add(selectedSuit);
+  item.innerHTML = rank[getRndInteger(rank.length)];
 };
+
+function getRndInteger(limit) {
+  return Math.floor(Math.random() * limit);
+}
